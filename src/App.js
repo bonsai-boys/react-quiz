25f0a46552284701508573f77f3c0de517458efe
MatiasTontti <matias.tontti@gmail.com>
2018-01-24 12:04:21 +0200

    tab esimerkki lisätty

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import { AppBar } from 'material-ui';
+import TabComponent from './TabComponent';
 
 const App = () => (
   <MuiThemeProvider>
+    <TabComponent/>
+    <MyAwesomeReactComponent />
     <MyAwesomeReactComponent />
   </MuiThemeProvider>
 );
